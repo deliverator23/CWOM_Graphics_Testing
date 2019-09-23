52a3a8b10ce1230d10bf2c14a5a1e12801b0df1f
iainm <terramirabilis1>
2019-09-23 14:59:34 +0100

    Latest changes.

--- CWON_Graphics_Testing/Gameplay_Text_Icons/Brandenburg_Gate_Gameplay.sql
+++ CWON_Graphics_Testing/Gameplay_Text_Icons/Brandenburg_Gate_Gameplay.sql
@@ -1,5 +1,3 @@
-
-
 INSERT INTO Types (Type, Kind) VALUES ('BUILDING_BRANDENBURG_GATE', 'KIND_BUILDING');
 
 INSERT INTO Buildings (BuildingType, Name, Description, PrereqTech, Cost, AdvisorType, MaxWorldInstances, IsWonder, RequiresPlacement, Quote, AdjacentDistrict)
@@ -7,7 +5,7 @@ VALUES ('BUILDING_BRANDENBURG_GATE', 'LOC_BUILDING_BRANDENBURG_GATE_NAME', 'LOC_
 
 --INSERT INTO BuildingPrereqs (Building, PrereqBuilding) VALUES ('BUILDING_BRANDENBURG_GATE', 'BUILDING_BANK');
 
---INSERT INTO Building_YieldChanges (BuildingType, YieldType, YieldChange) VALUES  ('BUILDING_BRANDENBURG_GATE', 'YIELD_GOLD', 2);
+INSERT INTO Building_YieldChanges (BuildingType, YieldType, YieldChange) VALUES  ('BUILDING_BRANDENBURG_GATE', 'YIELD_CULTURE', 2);
 
 INSERT INTO Building_ValidTerrains (BuildingType, TerrainType)
 VALUES
@@ -22,6 +20,21 @@ VALUES
   );
 
 
+-- +25% combat experience for all land units trained in this city
+INSERT INTO RequirementSets (RequirementSetId, RequirementSetType)
+VALUES ('BRANDENBURG_GATE_LAND_REQUIREMENTS', 'REQUIREMENTSET_TEST_ALL');
+
+INSERT INTO RequirementSetRequirements (RequirementSetId, RequirementId)
+VALUES ('BRANDENBURG_GATE_LAND_REQUIREMENTS', 'REQUIRES_LAND_DOMAIN');
+
+INSERT INTO Modifiers (ModifierId, ModifierType, SubjectRequirementSetId, Permanent)
+VALUES ('BRANDENBURG_GATE_TRAINED_UNIT_XP_MODIFIER', 'MODIFIER_CITY_TRAINED_UNITS_ADJUST_XP_BONUS', 'BRANDENBURG_GATE_LAND_REQUIREMENTS', 1);
+
+INSERT INTO ModifierArguments (ModifierId, Name, Value) VALUES ('BRANDENBURG_GATE_TRAINED_UNIT_XP_MODIFIER', 'Amount', '25');
+
+INSERT INTO BuildingModifiers (BuildingType, ModifierId) VALUES ('BUILDING_BRANDENBURG_GATE', 'BRANDENBURG_GATE_TRAINED_UNIT_XP_MODIFIER');
+
+
 -- 1 free Great General appears near the city where the wonder was built
 INSERT INTO Modifiers (ModifierId, ModifierType,  RunOnce, Permanent)
 VALUES ('BRANDENBURG_GRANT_GENERAL', 'MODIFIER_SINGLE_CITY_GRANT_GREAT_PERSON_CLASS_IN_CITY',  1, 1);
@@ -30,25 +43,3 @@ INSERT INTO ModifierArguments (ModifierId, Name, Value) VALUES ('BRANDENBURG_GRA
 INSERT INTO ModifierArguments (ModifierId, Name, Value) VALUES ('BRANDENBURG_GRANT_GENERAL', 'Amount', '1');
 
 INSERT INTO BuildingModifiers (BuildingType, ModifierId) VALUES ('BUILDING_BRANDENBURG_GATE', 'BRANDENBURG_GRANT_GENERAL');
-
-
-
---Any City with Art Museum yields + 1 Culture
---INSERT INTO Requirements (RequirementId, RequirementType) VALUES ('REQUIRES_CITY_HAS_ART_MUSEUM', 'REQUIREMENT_CITY_HAS_BUILDING');
-
---INSERT INTO RequirementArguments (RequirementId, Name, Value) VALUES ('REQUIRES_CITY_HAS_ART_MUSEUM', 'BuildingType', 'BUILDING_MUSEUM_ART');
-
---INSERT INTO RequirementSets (RequirementSetId, RequirementSetType) VALUES ('CITY_HAS_ART_MUSEUM_REQUIREMENTS', 'REQUIREMENTSET_TEST_ANY');
-
---INSERT INTO RequirementSetRequirements (RequirementSetId, RequirementId) VALUES ('CITY_HAS_ART_MUSEUM_REQUIREMENTS', 'REQUIRES_CITY_HAS_ART_MUSEUM');
-
-
---INSERT INTO Modifiers (ModifierId, ModifierType, SubjectRequirementSetId)
---VALUES ('UFFIZI_ART_MUSEUM_CULTURE_MODIFIER', 'MODIFIER_PLAYER_CITIES_ADJUST_CITY_YIELD_CHANGE', 'CITY_HAS_ART_MUSEUM_REQUIREMENTS');
-
-
---INSERT INTO ModifierArguments (ModifierId, Name, Value) VALUES ('UFFIZI_ART_MUSEUM_CULTURE_MODIFIER', 'YieldType', 'YIELD_CULTURE');
-
---INSERT INTO ModifierArguments (ModifierId, Name, Value) VALUES ('UFFIZI_ART_MUSEUM_CULTURE_MODIFIER', 'Amount', '1');
-
---INSERT INTO BuildingModifiers (BuildingType, ModifierId) VALUES ('BUILDING_BRANDENBURG_GATE', 'UFFIZI_ART_MUSEUM_CULTURE_MODIFIER');
